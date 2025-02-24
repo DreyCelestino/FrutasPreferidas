5f086c6c31e188a8ebb5a991ec87bc435bd30746
Andrey Celestino <lab53@fatec.com>
2025-02-24 11:06:05 -0300

    Finaliza programa com 5 frutas favoritas

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("|Top 5 Frutas Favoritas!");
+
+Console.WriteLine("|");
+Console.Write    ("|5. ");
+Console.WriteLine("Uva");
+Console.WriteLine("|Curto principalmente o suco.");
+
+Console.WriteLine("|");
+Console.Write    ("|4. ");
+Console.WriteLine("Manga");
+Console.WriteLine("|'Aqueles que sabem.'");
+
+Console.WriteLine("|");
+Console.Write    ("|3. ");
+Console.WriteLine("Limão");
+Console.WriteLine("|Umas gotas de limão vão bem em muita coisa!");
+
+Console.WriteLine("|");
+Console.Write    ("|2. ");
+Console.WriteLine("Maçã");
+Console.WriteLine("|Clássica, que infelizmente não estou podendo comer.");
+
+Console.WriteLine("|");
+Console.Write    ("|1. ");
+Console.WriteLine("Banana");
+Console.WriteLine("|Mais por memórias, saudades de comer banana com aveia quando pequeno.");
